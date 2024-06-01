7571c8b043eb52f76a2599c724be64b7387bba4b
WeinanLin <lwnpku2015@gmail.com>
2024-06-01 22:37:24 +0800

    Adamsp Some improvements

--- Adamsp/main.cpp
+++ Adamsp/main.cpp
@@ -4,13 +4,18 @@
 #include "algebras/utility.h"
 #include <iostream>
 
-int main_test(int, char**, int&, const char*);
+int main_test_associativity(int, char**, int&, const char*);
+int main_mul(int, char**, int&, const char*);
+int main_mul_v2(int, char**, int&, const char*);
+
 
 int main(int argc, char** argv)
 {
     bench::Timer timer;
     myio::SubCmdArg1d subcmds = {
-        {"test", "test", main_test},
+        {"test_assoc", "Test associativity", main_test_associativity},
+        {"mul", "Multiply two Milnor basis", main_mul},
+        {"mul_v2", "Multiply two Milnor basis using (c: E: R:) formatted input", main_mul_v2}
     };
     int index = 1;
     if (int error = myio::ParseSubCmd(argc, argv, index, PROGRAM, "Build R-motivic A-resolutions and chain maps.", VERSION, subcmds))
